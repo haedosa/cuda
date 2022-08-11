8b74db1c2294dec2ad90a09913e3e273913f6b9c
Taeho Son <sth0622@haedosa.xyz>
2022-08-11 21:52:31 +0900

    [BROKEN] nix build resolved, cabal build [WIP]

--- overlay.nix
+++ overlay.nix
@@ -3,8 +3,21 @@ final: prev: with final; {
   haskellPackages = prev.haskell.packages.ghc922.override (old: {
     overrides = lib.composeManyExtensions [
                   (old.overrides or (_: _: {}))
-                  (hfinal: hprev: {
-                    cuda = hfinal.callCabal2nix "cuda"  ./. { };
+                  (hfinal: hprev: with haskell.lib; rec {
+                    cuda =
+                      let
+                        pkg = hfinal.callCabal2nix "cuda"  ./. { };
+                      in
+                        overrideCabal pkg (drv: {
+                          extraLibraries = (drv.extraLibraries or []) ++ [pkgs.linuxPackages.nvidia_x11];
+                          configureFlags = (drv.configureFlags or []) ++ [
+                            "--extra-lib-dirs=${pkgs.cudatoolkit.lib}/lib"
+                            "--extra-include-dirs=${pkgs.cudatoolkit}/include"
+                          ];
+                          preConfigure = ''
+                            export CUDA_PATH=${pkgs.cudatoolkit}
+                        #   '';
+                        });
                   })
                 ];
   });
